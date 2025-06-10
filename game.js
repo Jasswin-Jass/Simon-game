e9d8f305cb1bf476409fbe4cb9cc5756e2241ae3
Jasswin-Jass <jasswinjass@gmail.com>
2025-06-10 16:33:07 +0530

    Added Files

---
+++ game.js
@@ -0,0 +1,94 @@
+
+
+buttonColors = ["red", "blue", "green", "yellow"]
+
+var gamePattern = []
+
+var userClickedPattern = []
+
+var started = false
+
+var level = 0
+
+function nextSequence() {
+
+    userClickedPattern = []
+    
+    level++
+
+    $("h1").text("level " + level)
+
+    var randomNumber = Math.floor(Math.random() * 4)
+    
+    randomChosenColor = buttonColors[randomNumber]
+
+    gamePattern.push(randomChosenColor)
+
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor)    
+
+}
+
+
+$(".btn").on("click", function() {
+    userChosenColor = $(this).attr("id")
+    userClickedPattern.push(userChosenColor)
+    playSound(userChosenColor)
+    pressAnimation(userChosenColor)
+    checkAnswer(userClickedPattern.length - 1)
+})
+
+function playSound(color) {
+    var audio = new Audio('sounds/' + color + '.mp3')
+    audio.play()
+}
+
+function pressAnimation(currentColor) { 
+    $("#" + currentColor).addClass("pressed")
+
+    setTimeout(function () { 
+        $("#" + currentColor).removeClass("pressed");
+     }, 100)
+ }
+
+ $(document).on("keypress", function () { 
+    $("h1").text("level " + level)
+    nextSequence()
+    started = true
+
+  })
+
+function checkAnswer(currentLevel) { 
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        console.log("success")
+        
+        if (userClickedPattern.length === gamePattern.length){
+
+            setTimeout(function(){
+                nextSequence()
+            }, 1000)
+        }
+    }
+    else {
+        playSound("wrong")
+        console.log("wrong")
+        $("body").addClass("game-over")
+        setTimeout(function(){
+            $("body").removeClass("game-over")
+        }, 200)
+        $("h1").text("Game Over, Press Any key to Restart")
+        startOver()
+    }
+ }
+
+function startOver() {
+    level = 0
+    gamePattern = []
+    started = false
+}
+
+
+
+
+
